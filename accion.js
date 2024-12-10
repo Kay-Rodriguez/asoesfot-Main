c84eea57f216b5c74046549f903145cb910f15b6
Kay-Rodríguez <karla.rodriguez01@epn.edu.ec>
2024-12-10 09:18:55 -0500

    first commit

---
+++ accion.js
@@ -0,0 +1,12 @@
+alert ("BIENVENIDO A LA ESFOT")
+var val1=2;
+var val2=3;
+var suma;
+suma=val1+val2;
+ alert("la suma es "+ suma);
+
+ var val1=2;
+ var val2=3;
+ var suma;
+ suma=val1+val2;
+  alert("la suma es "+ suma);
